c5ff32a77930661b24a82433f0bac79952cc3c3d
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2023-12-01 19:47:34 +0100

    Add dashboard

---
+++ src/dashboard.py
@@ -0,0 +1,123 @@
+import dash_mantine_components as dmc
+from dash import Dash, html, Input, Output, State, callback, dcc, dash_table
+import plotly.express as px
+import json
+from utils import get_data_files, load_df, get_vehicule_ids, COLS
+
+data = None
+
+app = Dash(
+    __name__,
+    external_stylesheets=[
+        # include google fonts
+        "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;900&display=swap"
+    ]
+)
+
+app.layout = dmc.MantineProvider(
+    theme={
+        "fontFamily": "'Inter', sans-serif",
+        "primaryColor": "indigo",
+        "components": {
+            "Button": {"styles": {"root": {"fontWeight": 400}}},
+            "Alert": {"styles": {"title": {"fontWeight": 500}}},
+            "AvatarGroup": {"styles": {"truncated": {"fontWeight": 500}}},
+        },
+    },
+    inherit=True,
+    withGlobalStyles=True,
+    withNormalizeCSS=True,
+    children=[
+        dmc.Container(
+            className="app-container",
+            fluid=True,
+            p=0,
+            children=[
+                dmc.Header(
+                    p="xs",
+                    height="auto",
+                    children=[
+                        dmc.Title("Cool Train", order=1, size=25, variant="gradient")
+                    ]
+                ),
+                dmc.Container(
+                    className="main",
+                    fluid=True,
+                    p=0,
+                    children=[
+                        dmc.Aside(
+                            className="aside",
+                            p="xs",
+                            width={"base": 300},
+                            withBorder=False,
+                            children=[
+                                dmc.Select(
+                                    label="Select dataset",
+                                    placeholder="Select one",
+                                    id="dataset-select",
+                                    data=get_data_files(),
+                                ),
+                                dmc.MultiSelect(
+                                    label="Select features",
+                                    placeholder="",
+                                    searchable=True,
+                                    clearable=True,
+                                    id="features-select",
+                                    data=COLS
+                                ),
+                                dmc.MultiSelect(
+                                    label="Select trains",
+                                    placeholder="",
+                                    searchable=True,
+                                    clearable=True,
+                                    id="trains-select",
+                                    data=[]
+                                )
+                            ]
+                        ),
+                        dmc.Container(
+                            className="content",
+                            fluid=True,
+                            p="xs",
+                            children=[
+                                # html.Div(dash_table.DataTable(
+                                #     id="full-data",
+                                #     sort_action="native",
+                                #     sort_mode="single",
+                                #     page_size=10,
+                                #     style_cell={'textAlign': 'left'},
+                                #     style_table={'height': 'auto', 'overflowY': 'auto', 'overflowX': 'auto'}, 
+                                # )),
+                                # dcc.Graph(figure={}, id='logs-count-per-veh')
+                            ]
+                        )
+                    ]
+                ),
+                
+            ]
+        ),
+        dcc.Store(id="dataset-filter", data=""),
+        dcc.Store(id="vehicules-filter", data=json.dumps([])),
+        dcc.Store(id="features-filter", data=json.dumps([]))
+    ],
+)
+
+@callback(Output("dataset-filter", "data"), Input("dataset-select", "value"))
+def select_data_file(value):
+    return value
+
+@callback(Output("trains-select", "data"), Input("dataset-select", "value"))
+def select_data_file(value):
+    return get_vehicule_ids(load_df(value))
+
+@callback(Output("features-filter", "data"), Input("features-select", "value"))
+def select_features(value):
+    return value
+
+@callback(Output("vehicules-filter", "data"), Input("trains-select", "value"))
+def select_features(value):
+    return value
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/utils.py
@@ -0,0 +1,68 @@
+from glob import glob
+from os.path import join, dirname, basename
+from os import listdir
+import pandas as pd
+import numpy as np
+from io import StringIO
+import json
+
+COLS = [
+    "mapped_veh_id",
+    "RS_E_OilPress_PC2",
+    "RS_E_OilPress_PC1",
+    "RS_E_RPM_PC1",
+    "RS_E_RPM_PC2",
+    "RS_E_InAirTemp_PC1",
+    "RS_E_InAirTemp_PC2",
+    "RS_E_WatTemp_PC1",
+    "RS_E_WatTemp_PC2",
+    "RS_T_OilTemp_PC1",
+    "RS_T_OilTemp_PC2"
+]
+
+COL_TYPES = {"mapped_veh_id": np.int32,
+            #"RS_E_OilPress_PC1": np.int32,
+            #"RS_E_OilPress_PC2": np.int32,
+            #"RS_E_RPM_PC1": np.int32,
+            #"RS_E_RPM_PC2": np.int32,
+            #"RS_E_InAirTemp_PC1": np.int32,
+            #"RS_E_InAirTemp_PC2": np.int32,
+            #"RS_E_WatTemp_PC1": np.int32,
+            #"RS_E_WatTemp_PC2": np.int32,
+            #"RS_T_OilTemp_PC1": np.int32,
+            #"RS_T_OilTemp_PC2": np.int32
+            }
+
+def get_data_files(dir=join(dirname(dirname(__file__)), "data")):
+    return [{"value": f, "label": basename(f)} for f in glob(join(dir, "*.csv"))]
+
+
+def load_df(file):
+    if not file:
+        return
+    return pd.read_csv(file, delimiter=";", index_col=0, dtype=COL_TYPES)
+
+def get_columns(df):
+    if df is None:
+        return []
+    return list(df)
+
+def get_vehicule_ids(df):
+    if df is None:
+        return []
+    return [str(e) for e in sorted(df["mapped_veh_id"].unique())]
+
+def df_to_dict(df):
+    if df is None:
+        return
+    return df.to_dict("records")
+
+def df_to_json(df):
+    if df is None:
+        return
+    return df.to_json(date_format='iso', orient='split')
+
+def json_to_df(data):
+    if not data:
+        return
+    return pd.read_json(StringIO(data), orient='split')
